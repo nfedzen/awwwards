154d788731b02d38e61ef5b7a9f55041b879760e
Nathan Fedzen <nfedzen@hotmail.com>
2025-05-28 00:48:17 -0600

    first push for awwwards website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <hi className="text-5xl text-orange-500 font-bold font-robert-regular">Welcome to Awwwards</hi>
+    </main>
+  )
+}
+
+export default App
